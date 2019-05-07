dc4870688a11591b94c97e8efda6c39281d41cd9
Sam Hodak <slhodak@gmail.com>
2019-05-07 12:22:38 -0700

    Implement json transfer through form

--- server/server.js
+++ server/server.js
@@ -9,17 +9,22 @@ const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-app.use(bodyParser.json());
+// const multer = require('multer');
+// app.use(bodyParser.json());
+// const upload = multer(); 
+const formidable = require('express-formidable');
+app.use(formidable());
 
 app.listen(3000);
-
+// app.use(upload.none());
 app.use(express.static(path.join(__dirname, path.normalize('../public'))));
 
-app.post('/giveMeJson', (req, res) => {  
+app.post('/', (req, res, next) => {  
+  console.log(req.fields);
   fs.readFile(path.join(__dirname, path.normalize('../public/index.html')), (err, data) => {
     if (!err) {
       res.status(200);
-      let template = '<p>blah blah blah</p>';
+      let template = '<p>' + req.fields + '</p>';
       res.end(data + template);
     } else {
       res.status(404);
