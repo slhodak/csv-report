c88cf438bd092ec8c621a8755fbbe4ebf6d928e5
Sam Hodak <slhodak@gmail.com>
2019-05-07 11:52:58 -0700

    Write response including form

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@
 //  send back report in response (along with form?)
 
 const path = require('path');
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -14,6 +15,15 @@ app.listen(3000);
 
 app.use(express.static(path.join(__dirname, path.normalize('../public'))));
 
-app.post('/giveMeJson', (req, res) => {
-  res.status(200).send(req.url);
+app.post('/giveMeJson', (req, res) => {  
+  fs.readFile(path.join(__dirname, path.normalize('../public/index.html')), (err, data) => {
+    if (!err) {
+      res.status(200);
+      let template = '<p>blah blah blah</p>';
+      res.end(data + template);
+    } else {
+      res.status(404);
+      res.end();
+    }
+  });
 });
