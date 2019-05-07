76d6de4a5f2108129a8f38a7e9d1c89bce43afee
Sam Hodak <slhodak@gmail.com>
2019-05-07 11:15:36 -0700

    Install express; set form action url

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Client app logic
 
 //  POST form data to server
-//    No jquery/ajax for submission (use request)
+//    No jquery/ajax for submission (use request?)
 //    Use default browser behavior (allow page reload/redirect...?)
 
--- server.js
+++ server.js
@@ -3,3 +3,11 @@
 //  receive json
 //  produce CSV report
 //  send back report in response (along with form?)
+
+
+const express = require('express');
+const app = express();
+
+app.post('/giveMeJson', (req, res) => {
+
+});
