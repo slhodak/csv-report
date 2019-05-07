eaf092d0a5f6beda6b2fcccb976cd4b82b4bb9e6
Sam Hodak <slhodak@gmail.com>
2019-05-07 15:12:34 -0700

    Implement getAllKeys

--- server/csv.js
+++ server/csv.js
@@ -7,19 +7,18 @@ module.exports = {
     //  for each of all of the keys, add the value from an object or add null
     //  end each object line with a '\n'
     let keys = module.exports.getAllKeys(json);
-    console.log(keys);
+
   },
   getAllKeys: function(json, result = []) {
     //  check the top level
     //  check each level
     for (var key in json) {
-      if (result.indexOf(key) < 0) {
-        result.push(key);
-      }
       if (key === "children") {
         for (var i = 0; i < key.length; i++) {
           module.exports.getAllKeys(key[i], result);
         }
+      } else if (result.indexOf(key) < 0 && key !== '0') {
+        result.push(key);
       }
     }
     return result;
--- serverSpec/serverSpec.js
+++ serverSpec/serverSpec.js
@@ -7,7 +7,7 @@ const testSuite = {
   },
   testGetAllKeys: function() {
     let actual = csv.getAllKeys(exampleData);
-    let expected = ['firstName, lastName, county, city, role, sales'];
+    let expected = ['firstName', 'lastName', 'county', 'city', 'role', 'sales'];
     console.log(actual);
     console.log(expected);
     console.assert((actual == expected), 'should list all keys from all levels of input json');
