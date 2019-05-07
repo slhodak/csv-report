a01fb52d75d77bc84aac15fc5b94b05ac8b49276
Sam Hodak <slhodak@gmail.com>
2019-05-07 11:31:03 -0700

    Reconfigure folder structure; serve static assets

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@
 //  produce CSV report
 //  send back report in response (along with form?)
 
-
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -12,6 +12,8 @@ app.use(bodyParser.json());
 
 app.listen(3000);
 
+app.use(express.static(path.join(__dirname, path.normalize('../public'))));
+
 app.post('/giveMeJson', (req, res) => {
-  res.status(200).send(req.body);
+  res.status(200).send(req.url);
 });
