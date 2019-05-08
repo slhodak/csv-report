47f387981c3bbb6064e2c84da5948c7decd04a9d
Sam Hodak <slhodak@gmail.com>
2019-05-07 18:39:21 -0700

    Final rabbit hole commit

--- server/csv.js
+++ server/csv.js
@@ -1,9 +1,24 @@
 module.exports = {
   jsonToCSV: function(json) {
-    
-    return json;
+    jsonArrays = module.exports.splitJSON(json);
+    module.exports.getAllKeys(jsonArrays);
   },
-  getAllKeys: function(json) {
-    
+  getAllKeys: function(jsonArrays) {
+    //  iterate through first object by checking for the first key repeating itself
+    let firstRow = jsonArrays[0].split(', ');
+    let keys = [];
+    for (var i = 0; i < firstRow.length; i++) {
+      if (keys.indexOf(firstRow[i].split(': ')[0]) < 0) {
+        keys.push(firstRow[i].split(': ')[0]) 
+      }
+    }
+    return keys;
+  },
+  splitJSON: function(jsonRaw) {
+    json = jsonRaw.replace(/{|}|\s\s+|"/g, '');
+    json = json.replace(/\[|\]/g, '');
+    json = json.replace(/\s\s/g, '');
+    json = json.split('children:');
+    return json;
   }
 };
--- serverSpec/serverSpec.js
+++ serverSpec/serverSpec.js
@@ -3,10 +3,27 @@ const csv = require('../server/csv');
 const testSuite = {
   runAllTests: function() {
     testSuite.testJSONtoCSV();
+    testSuite.testGetAllKeys();
+    testSuite.testSplitJSON();
   },
   testJSONtoCSV: function() {
     console.log('should convert JSON to csv');
     let actual = csv.jsonToCSV('{ "firstName": "Joshie", "lastName": "Wyattson", "county": "San Mateo", "city": "San Mateo", "role": "Broker", "sales": 1000000, "children": [ { "firstName": "Beth Jr.", "lastName": "Johnson", "county": "San Mateo", "city": "Pacifica", "role": "Manager", "sales": 2900000, "children": [ { "firstName": "Smitty", "lastName": "Won", "county": "San Mateo", "city": "Redwood City", "role": "Sales Person", "sales": 4800000, "children": [] }, { "firstName": "Allen", "lastName": "Price", "county": "San Mateo", "city": "Burlingame", "role": "Sales Person", "sales": 2500000, "children": [] } ] }, { "firstName": "Beth", "lastName": "Johnson", "county": "San Francisco", "city": "San Francisco", "role": "Broker/Sales Person", "sales": 7500000, "children": [] } ] };');
+    console.log(actual);
+  },
+  testSplitJSON: function() {
+    console.log('should split json into rows ending at object\'s last property');
+    let actual = csv.splitJSON('{ "firstName": "Joshie", "lastName": "Wyattson", "county": "San Mateo", "city": "San Mateo", "role": "Broker", "sales": 1000000, "children": [ { "firstName": "Beth Jr.", "lastName": "Johnson", "county": "San Mateo", "city": "Pacifica", "role": "Manager", "sales": 2900000, "children": [ { "firstName": "Smitty", "lastName": "Won", "county": "San Mateo", "city": "Redwood City", "role": "Sales Person", "sales": 4800000, "children": [] }, { "firstName": "Allen", "lastName": "Price", "county": "San Mateo", "city": "Burlingame", "role": "Sales Person", "sales": 2500000, "children": [] } ] }, { "firstName": "Beth", "lastName": "Johnson", "county": "San Francisco", "city": "San Francisco", "role": "Broker/Sales Person", "sales": 7500000, "children": [] } ] };');
+    console.log(actual);
+  },
+  testGetAllKeys: function() {
+    console.log('should get all keys');
+    let actual = csv.getAllKeys([ ' firstName: Joshie, lastName: Wyattson, county: San Mateo, city: San Mateo, role: Broker, sales: 1000000, ',
+    '   firstName: Beth Jr., lastName: Johnson, county: San Mateo, city: Pacifica, role: Manager, sales: 2900000, ',
+    '   firstName: Smitty, lastName: Won, county: San Mateo, city: Redwood City, role: Sales Person, sales: 4800000, ',
+    '  ,  firstName: Allen, lastName: Price, county: San Mateo, city: Burlingame, role: Sales Person, sales: 2500000, ',
+    '    ,  firstName: Beth, lastName: Johnson, county: San Francisco, city: San Francisco, role: Broker/Sales Person, sales: 7500000, ',
+    '    ;' ]);
     console.log(actual);
   }
 }
